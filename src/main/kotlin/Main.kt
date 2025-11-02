ecc0e717d7f33803f9f4f12f6b078e85a69a57e9
Kory | Ryosuke Kondo <korygm33@gmail.com>
2025-11-02 18:33:44 +0000

    chore: initialize simple gradle project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,3 @@
+fun main() {
+    println("Hello, Reversi Path Finder!")
+}
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class MainKtTest {
+    @Test
+    fun testBasic() {
+        assertTrue(true)
+    }
+}
